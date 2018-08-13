bfe69857e57027153c029eb9152df3bf67e10781
kim.jin <kim.jin@eisoo.com>
2018-08-13 10:33:23 +0800

    2.0

--- my_app/src/component/CommentInput.js
+++ my_app/src/component/CommentInput.js
@@ -1,6 +1,36 @@
 import React, { Component } from 'react'
 
 class CommentInput extends Component {
+    constructor (){
+        super()
+        this.state ={
+            username  : '',
+            content : ''
+        }
+    }
+    handleUsernameChange (event){
+        this.setState({
+            username : event.target.value
+        })
+    }
+    handleUsercontentChange (event){
+        this.setState({
+            content : event.target.value
+        })
+    }
+    handleSubmit (){
+        if(this.props.onSubmit){
+           this.props.submit({
+            username:this.state.username,
+            content:this.state.content,
+           })
+        }
+        this.setState({
+            content:'',
+            username:'',
+        })
+    }
+
     render() {
         return (
             <div>
@@ -8,17 +38,17 @@ class CommentInput extends Component {
                     <div className='comment-field'>
                         <span className='comment-field-name'>用户名：</span>
                         <div className='comment-field-input'>
-                            <input />
+                            <input value = {this.state.username} onChange={this.handleUsernameChange.bind(this)}/>
                         </div>
                     </div>
                     <div className='comment-field'>
                         <span className='comment-field-name'>评论内容：</span>
                         <div className='comment-field-input'>
-                            <textarea />
+                            <textarea value = {this.state.content} onChange ={this.handleUsercontentChange.bind(this)}/>
                         </div>
                     </div>
                     <div className = 'comment-field-button'>
-                        <button>发11布</button>
+                        <button onClick = {this.handleSubmit.bind(this)}>发布</button>
                     </div>
                 </div>
             </div>
--- my_app/src/component/CommentList.js
+++ my_app/src/component/CommentList.js
@@ -1,9 +1,17 @@
 import React ,{Component} from 'react'
+import Comment from './Comment'
 
 class CommentList extends Component {
     render(){
+        const comments =[
+            {username :'kim',content :'hello'},
+            {username :'kibana', content :'worls'},
+            {username :'john' , content : 'good'}
+        ]
         return (
-            <div> CommentList </div>
+            <div>
+            {comments.map((comment,i)=><Comment comment={comment} key ={i} />)}
+            </div>
         )
     }
 }
