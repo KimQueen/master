6ba08221ef2e66f3fc081393066098d3f83c1cf9
kim.jin <kim.jin@eisoo.com>
2018-12-07 10:56:15 +0800

    重新安装

--- my_app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-//import App from './App';
-import CommentApp from './component/CommentApp'
-//import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<CommentApp/>, document.getElementById('root'))
-//registerServiceWorker();
